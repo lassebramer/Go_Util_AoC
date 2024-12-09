756fe3b5d0d7f055b1be48bdb00c5c4368fc3ece
Lasse Bramer Schmidt <lassebramer@gmail.com>
2024-12-09 10:10:04 +0100

    module done

---
+++ Go_Util_AoC.go
@@ -0,0 +1,26 @@
+package Go_Util_AoC
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func ReadInput(inputFile string) []string {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var inputArray []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputArray = append(inputArray, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return inputArray
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lassebramer/Go_Util_AoC
+
+go 1.23.3
